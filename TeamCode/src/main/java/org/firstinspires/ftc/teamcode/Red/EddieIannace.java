9df78ec41dd7037a218f3a7f1d2bcad8d94b4902
iannacee28709 <iannacee28709@student.cghsnc.org>
2025-10-28 14:46:37 -0400

    Eddie Iannace

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red/EddieIannace.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red/EddieIannace.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.Red;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-//autono
+//auto
 import org.firstinspires.ftc.teamcode.autonomous.AbstractAutonomous;
 
 @Autonomous(name = "EddieIannace")
