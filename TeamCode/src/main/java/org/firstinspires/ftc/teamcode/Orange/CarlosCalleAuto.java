aa147980aea9cc5f71d8a8d653894bd3b1244e31
callec26128 <callec26128@student.cghsnc.org>
2025-10-28 14:26:52 -0400

    Carlos Calle

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Orange/CarlosCalleAuto.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.Orange;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.autonomous.AbstractAutonomous;
+
+@Autonomous(name = "CarlosCalleAuto")
+public class CarlosCalleAuto extends AbstractAutonomous {
+
+    @Override
+    public void runOpMode(){
+        robot.init(this.hardwareMap, telemetry);
+        waitForStart();
+        turnToAngle(90);
+
+
+    }
+}
+
