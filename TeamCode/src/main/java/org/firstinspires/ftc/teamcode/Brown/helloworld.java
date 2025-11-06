2c2e70b538968635c8d0771dc42b0063c21c03bc
finel26470 <finel26470@student.cghsnc.org>
2025-11-06 14:49:00 -0500

    lukefine

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Brown/helloworld.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.Brown;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp public class helloworld extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addLine("hello world");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
