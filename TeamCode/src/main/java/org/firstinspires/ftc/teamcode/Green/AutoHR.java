254614763b3b7638d42e77f5cf2a3bbba1dbe2cc
Haley Rankin <rankinh28524@student.cghsnc.org>
2025-11-06 14:33:58 -0500

    Haley Rankin's Hello

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Green/AutoHR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Green/AutoHR.java
@@ -3,23 +3,19 @@ package org.firstinspires.ftc.teamcode.Green;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.autonomous.AbstractAutonomous;
-import org.firstinspires.ftc.teamcode.hardware.RobotHardware;
 
 @Autonomous(name="HaleyRankinAuto")
-public class HaleyRankinAuto extends AbstractAutonomous {
+public class AutoHR extends AbstractAutonomous {
     @Override
-    public void runOpMode(){
+    public void runOpMode() {
         robot.init(this.hardwareMap, telemetry);
         waitForStart();
-        move(10,0);
+        move(10, 0);
         turnRelative(90);
-        move(10,0);
+        move(10, 0);
         turnRelative(90);
-        move(10,0);
+        move(10, 0);
         turnRelative(90);
-        move(10,0);
-
-
-
+        move(10, 0);
     }
 }
