f810c84ec46befbd4cd94e162f87295489467f40
BB Musasa <musasab26621@student.cgshsnc.org>
2025-11-06 14:25:21 -0500

    BB Musasa

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Orange/BBMusasaHelloworld.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.Orange;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@TeleOp public class BBMusasaHelloworld extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addLine("hello world");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
