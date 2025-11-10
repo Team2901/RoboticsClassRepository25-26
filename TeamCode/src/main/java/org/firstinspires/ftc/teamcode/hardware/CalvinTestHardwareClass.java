57613c3c875fcfbd453ccdb91d0b02c2efd766b8
landrya27384 <landrya27384@student.cghsnc.org>
2025-11-10 14:20:18 -0500

    Updated CalvinTestHardwareClass so motors can go

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/CalvinTestHardwareClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/CalvinTestHardwareClass.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -14,5 +16,15 @@ public class CalvinTestHardwareClass {
         leftMotor = hardwareMap.get(DcMotorEx.class,"leftMotor");
         rightMotor = hardwareMap.get(DcMotorEx.class, "rightMotor");
         claw = hardwareMap.get(Servo.class, "claw");
+
+        leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //claw = hardwareMap.servo.get("claw");
+        //claw.setPosition(0);
     }
 }
