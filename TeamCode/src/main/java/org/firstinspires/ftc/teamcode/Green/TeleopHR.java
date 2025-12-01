da2b8bd1247fd27a46ae6c1bede703f232f66cd1
Haley Rankin <rankinh28524@student.cghsnc.org>
2025-12-01 14:06:50 -0500

    hardware map edit -> misspelled "harwareMap"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Green/TeleopHR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Green/TeleopHR.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.Green;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.hardware.ImprovedGamepad;
@@ -12,28 +13,29 @@ public abstract class TeleopHR extends OpMode  {
     public OutreachHardware robot = new OutreachHardware();
     ImprovedGamepad gamepad;
 
+    private HardwareMap hardwareMap;
     @Override
     public void init() {
         gamepad = new ImprovedGamepad(gamepad1, new ElapsedTime(), "Gamepad");
-        robot.init((this.hardwareMap));
+        robot.init(this.hardwareMap);
     }
 
-        public void loop() {
-            gamepad.update();
+    public void loop() {
+        gamepad.update();
 
-            double leftMotorPower = 0;
-            double rightMotorPower = 0;
-            leftMotorPower += gamepad.left_stick_y.getValue() / 2;
-            rightMotorPower += gamepad.left_stick_y.getValue() / 2;
+        double leftMotorPower = 0;
+        double rightMotorPower = 0;
+        leftMotorPower += gamepad.left_stick_y.getValue() / 2;
+        rightMotorPower += gamepad.left_stick_y.getValue() / 2;
 
-            if(gamepad.right_stick_x.getValue() > 0) {
-                leftMotorPower += gamepad.right_stick_x.getValue() / -2;
-            } else {
-                rightMotorPower += Math.abs(gamepad.right_stick_x.getValue() / -2);
-            }
+        if(gamepad.right_stick_x.getValue() > 0) {
+            leftMotorPower += gamepad.right_stick_x.getValue() / -2;
+        } else {
+            rightMotorPower += Math.abs(gamepad.right_stick_x.getValue() / -2);
+        }
 
-            robot.leftDrive.setPower(leftMotorPower);
-            robot.rightDrive.setPower(rightMotorPower);
+        robot.leftDrive.setPower(leftMotorPower);
+        robot.rightDrive.setPower(rightMotorPower);
         }
     }
 
