1633465ed2305f3e793cb6e174d9cb488733982e
Haley Rankin <rankinh28524@student.cghsnc.org>
2025-12-01 13:27:28 -0500

    TeleopHR.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Green/TeleopHR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Green/TeleopHR.java
@@ -15,7 +15,7 @@ public abstract class TeleopHR extends OpMode  {
     @Override
     public void init() {
         gamepad = new ImprovedGamepad(gamepad1, new ElapsedTime(), "Gamepad");
-        robot.init(this.hardwareMap);
+        robot.init((this.hardwareMap));
     }
 
         public void loop() {
