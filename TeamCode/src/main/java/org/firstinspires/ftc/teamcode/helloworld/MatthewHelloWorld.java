358b34009f738c792a433f8d0de04a7570aa6529
Sarah Acevedo <acevedos2800@student.cghsnc.org>
2025-12-03 14:16:43 -0500

    Sarah Acevedo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helloworld/MatthewHelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helloworld/MatthewHelloWorld.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
-class MatthewHelloWorld extends OpMode {
+public class MatthewHelloWorld extends OpMode {
     @Override
     public void init() {
         telemetry.addLine("Hello World");
