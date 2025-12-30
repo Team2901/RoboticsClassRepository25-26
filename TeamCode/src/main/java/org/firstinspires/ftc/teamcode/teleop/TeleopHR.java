f3a88772d7003b686779a25a0d42bcafe5d580c8
mcgeeo27445 <mcgeeo27445@student.cghsnc.org>
2025-12-30 16:42:03 -0500

    moved teleop classes to teleop folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopHR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopHR.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Orange;
+package org.firstinspires.ftc.teamcode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -7,31 +7,33 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.hardware.ImprovedGamepad;
 import org.firstinspires.ftc.teamcode.hardware.OutreachHardware;
 
-@TeleOp(name="CarlosCalleTeleop")
-public class CarlosCalleTeleop extends OpMode {
+@TeleOp (name="TeleopHR")
+public class TeleopHR extends OpMode  {
     public OutreachHardware robot = new OutreachHardware();
     ImprovedGamepad gamepad;
 
-    public void init(){
-        gamepad=new ImprovedGamepad(gamepad1, new ElapsedTime(), "Gamepad");
+    @Override
+    public void init() {
+        gamepad = new ImprovedGamepad(gamepad1, new ElapsedTime(), "Gamepad");
         robot.init(this.hardwareMap);
     }
-    public void loop(){
+    @Override
+    public void loop() {
         gamepad.update();
 
-        double leftMotorPower = 0 ;
+        double leftMotorPower = 0;
         double rightMotorPower = 0;
-
         leftMotorPower += gamepad.left_stick_y.getValue() / 2;
         rightMotorPower += gamepad.left_stick_y.getValue() / 2;
 
-        if(gamepad.right_stick_x.getValue() > 0) {
-            leftMotorPower += gamepad.right_stick_x.getValue() / 2;
+        if(gamepad.right_stick_x.getValue() < 0) {
+            leftMotorPower += gamepad.right_stick_x.getValue() / -2;
         } else {
-            rightMotorPower += Math.abs(gamepad.right_stick_x.getValue() /2);
+            rightMotorPower += Math.abs(gamepad.right_stick_x.getValue() / -2);
         }
 
         robot.leftDrive.setPower(leftMotorPower);
         robot.rightDrive.setPower(rightMotorPower);
+        }
     }
-}
+
